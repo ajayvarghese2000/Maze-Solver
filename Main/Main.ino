4e3649273952e52b17996a9a0a142ac4c77ba9f2
ajayvarghese2000 <58085441+ajayvarghese2000@users.noreply.github.com>
2021-04-12 10:54:30 +0100

    Commented the main file

--- Main/Main.ino
+++ Main/Main.ino
@@ -7,18 +7,18 @@
 #include <Servo.h> // Library to Control the Motors
 
 
-#include <hcsr04.h> // Incluing Library for Easy Ultrasonic Sernsor Ativation
+#include <hcsr04.h> // Including Library for Easy Ultrasonic Sensor Activation
 
 /*
     To use the ultrasonic library you first need to define an US entity
 
         HCSR04 <Name of Variable>(Trigger Pin, Echo Pin, MinRange, MaxRange);
     
-    Then you have access to subfunctions, to get the distance in millimeters do:
+    Then you have access to sub functions, to get the distance in millimetres do:
         
         <Name of Variable>.distanceInMillimeters();
     
-    This will retun the distance in mm as an int if its over the max range it will output -1
+    This will return the distance in mm as an int if its over the max range it will output -1
 
 */
 
@@ -43,7 +43,7 @@ HCSR04 USR(4, 5, 10, 100);
 // Creating the accell and mag A4 and A5 
 LSM303 compass;
 
-// Creating the Servo entitys - Will be attached to the pins 11 and 10 in setup()
+// Creating the Servo entities - Will be attached to the pins 11 and 10 in setup()
 Servo servoleft;
 Servo servoright;
 
@@ -54,7 +54,7 @@ int led2 = 12; // Left
 // Pause Switch
 int Pause = 9;
 
-// Run Switchs Default High, Low when Active
+// Run Switch's Default High, Low when Active
 int R1 = 8;
 int R2 = 7;
 int R3 = 6;
@@ -64,17 +64,24 @@ int R3 = 6;
 // Global Variables
 //      Define any global variables you need here
 // Array of 100 chars to store the directions the robot takes on each runs
+/*
+    Future Ajay here :  The stuff below is no longer used as described.
+                        New Use = After each turn, the number in the nodes variable increases
+                        During run 3 the program looks at which one has less nodes (i.e less turns) and takes that
+                        this longer requires logging the turns and solving so is faster, albeit simple.
+                        Not deleting anything as that will probably mess things beyond my understanding up.
+*/
 struct runs{
     char turns[100];  // e.g 
     bool valid;      //      turns[100] = {'l', 'r', 'r', 'l','e', 'e', 'e'}
     int t_loc;      // l/r for left/right and e for end/null
-    int nodes;
+    int nodes;  // Number of turns taken
 }; // Valid gets set to true after the run is complete and t-loc is the location of the T junction in the run array
 
-runs run[2];
+runs run[2]; // Need two instances as we need to collect data for 2 runs
 
-// Run selector deictates what run the robot is doing. Will be controlled by an external 3-pole switch attached to the uno
-boolean runselect[2] = {false,false};
+// Run selector dictates what run the robot is doing. Will be controlled by an external 3-pole switch attached to the Uno
+boolean runselect[2] = {false,false}; // Check the updateRunSelect() function for more info
 
 
 
@@ -107,36 +114,34 @@ void setup(){
     pinMode(led, OUTPUT);
     pinMode(led2, OUTPUT);
 
+    // Making sure the boebot doesn't accidently move without the pause switch command
     boebot_stop();
 
 }
 
 void loop() {
 
+    // When pause switch is low, the boebot will start to operate 
     while (PauseActive() == false)
     {
-        updateRunSelect();
-        solvemaze();
-        //Serial.println(GetCurrentAngle());
-        //Serial.println(boebot_sensor(2));
-        //follow_wall();
-        //testing();
-        //updateRunSelect();
-        //LightLED(2);
+        updateRunSelect();  // Checks what run we're on currently
+        solvemaze();        // Starts the maze solver
     }
 
 }
 
-// Takes an avarage of 15 values from the sensor to combat inaccuracies
-// Takes the sensor reqired and retuns the avrage value in mm
+// Takes an average of 15 values from the sensor to combat inaccuracies
+// Takes the sensor required and returns the average value in mm
+// Future Ajay here :   pretty sure none of this code is used, taking and average
+//                      took too long and wasn't needed. Too scared to delete though.
 // 0 = front, 1 = left, 2 = right
 int boebot_sensor(int sensor){
     
     // Variable to store the total (Gets reset on each call)
     int total = 0;
     
-    // Switch statment to pick which sensor to ping
-    // 0 = Front; 1 = Left; 2 = Right; anyother will return an error code of -1 
+    // Switch statement to pick which sensor to ping
+    // 0 = Front; 1 = Left; 2 = Right; any other will return an error code of -1 
     switch (sensor)
     {
     case 0: // Front Sensor
@@ -146,7 +151,7 @@ int boebot_sensor(int sensor){
         {
             // Pulsing the US Sensor and adding it to the current total
             total = IRSensorDistance() + total;
-            delay(5); // Delay to not onverload the module
+            delay(5); // Delay to not overload the module
         }
 
         // Getting the average
@@ -163,7 +168,7 @@ int boebot_sensor(int sensor){
         {
             // Pulsing the US Sensor and adding it to the current total
             total = USL.distanceInMillimeters() + total;
-            delay(5); // Delay to not onverload the module
+            delay(5); // Delay to not overload the module
         }
 
         // Getting the average
@@ -180,7 +185,7 @@ int boebot_sensor(int sensor){
         {  
             // Pulsing the US Sensor and adding it to the current total
             total = USR.distanceInMillimeters() + total;
-            delay(5); // Delay to not onverload the module
+            delay(5); // Delay to not overload the module
         }
 
         // Getting the average
@@ -228,6 +233,7 @@ void boebot_turn_right(){
     
 }
 
+// Turns the robot 180 in the right direction
 void boebot_turn_180R(){
     servoleft.writeMicroseconds(1600);
     servoright.writeMicroseconds(1600);
@@ -235,6 +241,8 @@ void boebot_turn_180R(){
     boebot_stop();
     
 }
+
+// Turns the robot 180 in the left direction
 void boebot_turn_180L(){
     servoleft.writeMicroseconds(1400);
     servoright.writeMicroseconds(1400);
@@ -250,18 +258,20 @@ void boebot_stop(){
     return;
 }
 
-// An overall function that will run the solving
+// An overall function that will run the solving used to determine which algorithm to use
+// If the user wants to make a change this is where they can uses a different algorithm
 void solvemaze(){
     if ( runselect[1] == false && runselect[0] == true) // If the runsel == 01 i.e run 1
     {
-        lefthandrule();
+        lefthandrule(); // Goes left at the T-junction
     }
     else if ( runselect[1] == true && runselect[0] == false) // If the runsel == 11 i.e run 2
     {
-        righthandrule();
+        righthandrule(); // Goes right at the T-junction
     }
-    else{
-        FloodFill();
+    else
+    {
+        FloodFill(); // Checks which is faster
     }
     return;
     
@@ -271,7 +281,7 @@ void solvemaze(){
 void clearruns(){
     for (int i = 0; i < 2; i++) // for each run
     {
-        run[i].t_loc = -1; // Setting an inaccesable value for the T-locaction
+        run[i].t_loc = -1; // Setting an inaccessible value for the T-location
         run[i].valid = false; // Setting the run to be invalid
         
         for (int j = 0; j < 100; j++) // for each 100 turns in each run
@@ -281,12 +291,13 @@ void clearruns(){
         
     }
 
-    run[0].nodes = 0;
-    run[1].nodes = 0;
+    run[0].nodes = 0; // setting the number of turns to to 0 in run 1
+    run[1].nodes = 0; // setting the number of turns to to 0 in run 2
     
 }
 
 // When run it will update the global runselect variable and print which run is active
+// Checks what run we're on so the right algorithm is used
 void updateRunSelect(){
 
     // Gets the state of the pins by doing a digital read
@@ -341,39 +352,45 @@ boolean PauseActive(){
     }
 }
 
+// Variable to keep track of what turn was done previously.
+// This way we know which way to turn if we need to turn 180.
 int LastTurn = 0;
 
 // Function to run the left hand rule.
 void lefthandrule(){
+    
+    // Checks if the front is blocked by something 
     if (IsFrontBlocked() == true)
     {
-        int node = AvailableTurns();
-        int left = USL.distanceInMillimeters();
-        int right = USR.distanceInMillimeters();
-        LightLED(node);
-        run[0].nodes = run[0].nodes + 1;
+        int node = AvailableTurns(); // Checks what turn we have available
+        int left = USL.distanceInMillimeters();     // Future Ajay here :   These are definitely not used,
+        int right = USR.distanceInMillimeters();    //                      Still too scared to remove, just in case
+        LightLED(node); // Lights up the LED's that correspond to the turn 
+        run[0].nodes = run[0].nodes + 1; // Adds one to the number of turns
+        
+        // Turns in the direction that is available to it
         switch (node)
         {
-        case 1:
-            boebot_turn_left();
-            LastTurn = 1;
+        case 1: // Left turn only
+            boebot_turn_left();     // turns left
+            LastTurn = 1;           // sets last turn to left
             break;
-        case 2:
-            boebot_turn_right();
-            LastTurn = 2;
+        case 2: // right turn only
+            boebot_turn_right();    // turns right
+            LastTurn = 2;           // sets last turn to right
             break;
-        case 3:
-            boebot_turn_left();
-            LastTurn = 1;
+        case 3: // T-junction
+            boebot_turn_left();     // turns left
+            LastTurn = 1;           // sets last turn to left
             break;
-        default:
-            deadend();
+        default: // dead-end
+            deadend();              // Runs the dead end logic
             break;
         }
     }
     else
     {
-        return;
+        return; // Returns back if the front is not blocked
     }
 }
 
@@ -383,14 +400,14 @@ boolean IsFrontBlocked(){
     // Checks if something is blocking the front sensor
     if(newdis > 45)
     {   
-        boebot_move_forwards();
-        follow_wall();
+        boebot_move_forwards(); // Sets the robot to go forwards
+        follow_wall();          // Wall alignment algorithm 
         return false;
         
     }
     else // Default action is to stop, so it fails to safe
     {
-        boebot_stop();
+        boebot_stop(); // Stops if the front is blocked
         return true;
     }    
 }
@@ -402,16 +419,16 @@ int IRSensorDistance(){
     return distance;
 }
 
-// Function the checks what turns are avaliable and returns a number
-// 0 = no turns, 1 = left turn only, 2 = right trun only, 3 = T junction
+// Function the checks what turns are available and returns a number
+// 0 = no turns, 1 = left turn only, 2 = right turn only, 3 = T junction
 int AvailableTurns(){
     int left = boebot_sensor(1); // get the left distance
     int right = boebot_sensor(2); // get the right distance
 
-    Serial.print("Left Sensor = ");
+    /*Serial.print("Left Sensor = ");
     Serial.print(left);
     Serial.print("right Sensor = ");
-    Serial.println(right);
+    Serial.println(right);*/
 
     if (left == -1 && right == -1) // T junction
     {
@@ -457,8 +474,9 @@ void LightLED(int node){
     }
 }
 
-// Uses the magnatometer to return the current angle in degrees it is facing.
+// Uses the magnetometer to return the current angle in degrees it is facing.
 // Remember angles are measured counter-clockwise
+// Future Ajay here :   Have fun rewriting this :))))
 double GetCurrentAngle(){
     
     compass.read();
@@ -470,7 +488,7 @@ double GetCurrentAngle(){
                                    {0.00461787, -0.00896044, 0.91744137}};
 
     Xm_off = compass.m.x*(100000.0/1100.0) - bias[0]; //X-axis combined bias (Non calibrated data - bias)
-    Ym_off = compass.m.y*(100000.0/1100.0) - bias[1]; //Y-axis combined bias (Default: substracting bias)
+    Ym_off = compass.m.y*(100000.0/1100.0) - bias[1]; //Y-axis combined bias (Default: subtracting bias)
     Zm_off = compass.m.z*(100000.0/980.0 ) - bias[2]; //Z-axis combined bias
 
     Xm_cal =  soft_hard_iron[0][0]*Xm_off + soft_hard_iron[0][1]*Ym_off + soft_hard_iron[0][2]*Zm_off; //X-axis correction for combined scale factors (Default: positive factors)
@@ -490,20 +508,21 @@ double GetCurrentAngle(){
 
 }
 
-void follow_wall(){
-    int left = USL.distanceInMillimeters();
-    int right = USR.distanceInMillimeters();
-    int delayt = 30;
-    //boebot_move_forwards();
-    if (left == -1 && right == -1)
-    {
-        //boebot_move_forwards();
 
+// Function that looks at the distances output by the US sensors and decides what direction to move.
+void follow_wall(){
+    int left = USL.distanceInMillimeters(); // gets the left distance without delays
+    int right = USR.distanceInMillimeters();// gets the right distance without delays
+    int delayt = 30; // Variable to set the length of time the robot turns away from the walls
+    //boebot_move_forwards();
+    if (left == -1 && right == -1) // Stops the robot when the sensors are erroring out, can happen
+    {                              // if the distance it too far or in a dead end / when there is only one wall etc
+        //boebot_move_forwards();
         boebot_stop();
     }
-    else if (left == -1)
+    else if (left == -1) // If the left is erroring, we check the right
     {
-        if (right < 55)
+        if (right < 55) // if the right is too close to the wall we move to the left for a small amount of time
         {
             //Serial.println("Need to turn Left");
             servoleft.writeMicroseconds(1400);
@@ -516,9 +535,9 @@ void follow_wall(){
         }
         
     }
-    else if (right == -1)
+    else if (right == -1) // If the right is erroring, we check the left
     {
-        if (left < 55)
+        if (left < 55) // if the left is too close to the wall we move to the right for a small amount of time
         {
             //Serial.println("Need to Turn Right");
             servoleft.writeMicroseconds(1590);
@@ -531,16 +550,16 @@ void follow_wall(){
         }
         
     }
-    else
+    else // If we can get readings from both sensors :
     {
-        if (right < left)
+        if (right < left) // if we are closer to the right than the left -> turn left
         {
             //Serial.println("Need to turn Left");
             servoleft.writeMicroseconds(1400);
             servoright.writeMicroseconds(1400);
             delay(delayt);
         }
-        else if(left < right)
+        else if(left < right) // if we are closer to the left than the right -> turn right
         {
             //Serial.println("Need to Turn Right");
             servoleft.writeMicroseconds(1590);
@@ -554,9 +573,11 @@ void follow_wall(){
         }
     }
     
-    boebot_move_forwards();
+    boebot_move_forwards(); // Sets the robot to go forwards once the corrections are made
 }
 
+
+// Future Ajay here :   This function can be binned
 void testing(){
     while (PauseActive() == false)
     {
@@ -574,17 +595,19 @@ void testing(){
     }
 }
 
+
+// Dead end logic, turns 180 left or right depending on the previous turn
 void deadend(){
 
-    if (LastTurn == 1)
+    if (LastTurn == 1) // if the last turn was left -> turn right
     {
         boebot_turn_180R();
     }
-    else if (LastTurn == 2)
+    else if (LastTurn == 2) // if the last turn was right -> turn left
     {
         boebot_turn_180L();
     }
-    else
+    else // Check where we have space
     {
         int left = USL.distanceInMillimeters();
         int right = USR.distanceInMillimeters();
@@ -601,6 +624,8 @@ void deadend(){
     }
 }
 
+
+// Same as lefthandrule() but right, sooo complex I know!
 void righthandrule(){
     if (IsFrontBlocked() == true)
     {
@@ -634,6 +659,8 @@ void righthandrule(){
     }
 }
 
+
+// Checks which of the two runs have more "nodes" (turns) and takes the one with less turns.
 void FloodFill(){
 
     if (run[1].nodes>run[0].nodes)
